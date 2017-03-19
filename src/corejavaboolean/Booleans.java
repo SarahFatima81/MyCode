89766d4e439eb0c02fa56dd89fad82a9834599b8
Sarah Fatima <sfatima81@gmail.com>
2017-03-18 19:15:05 -0500

    Add files via upload

---
+++ src/corejavaboolean/Booleans.java
@@ -0,0 +1,34 @@
+package corejavaboolean;
+
+public class Booleans {
+
+	public static void main(String[] args) {
+		
+		// Equal to               ==
+        // Greater than            >
+		// Less than               <
+	    // Greater than/equal to   >=
+		// Less than/equal to      <=
+		// Not equal to            !=
+		
+		boolean a = (5 == 5);
+			System.out.println(a);
+			
+		boolean x = (12 - 5 <= 4);
+		    System.out.println(x);
+		
+		
+		boolean prod = (9 * 9 != 81);
+		
+		if (prod) {
+			System.out.println("Condition is true");
+		}else{
+			System.out.println("Condition is false");
+			
+		  	
+		}
+			
+		
+	}
+
+}

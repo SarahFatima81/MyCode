f7550e7f844b63ca71549a17d8deea1438e4380e
Sarah Fatima <sfatima81@gmail.com>
2017-03-28 19:04:40 -0500

    Add files via upload
    Java Assignment - 03/18/2017

---
+++ src/corejavaarrays/ArrayTable.java
@@ -0,0 +1,20 @@
+package corejavaarrays;
+
+public class ArrayTable {
+
+	public static void main(String[] args) {
+		System.out.println("Index \t Table");
+		
+		int ageList[] = {22, 45, 60, 18, 32, 75};
+		
+		for(int counter = 0; counter < ageList.length; counter++){
+			System.out.println(counter + "  \t  " + ageList[counter]);
+		}
+		
+		
+		
+		
+
+	}
+
+}

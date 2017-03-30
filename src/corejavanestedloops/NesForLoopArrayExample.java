ab68749e55cc9fce3e9769105725b3fb612a31b7
Sarah Fatima <sfatima81@gmail.com>
2017-03-30 18:22:17 -0500

    Add files via upload
    Commit changes

--- src/corejavanestedloops/NesForLoopArrayExample.java
+++ src/corejavanestedloops/NesForLoopArrayExample.java
@@ -32,12 +32,12 @@ public class NesForLoopArrayExample {
             int ageHeightDetails1[][] = {{20, 6}, {35, 7}, {18, 5}};
             
             for(int i = 0; i < names.length; i++){
-            	System.out.println(names[i]);
+            	System.out.println(names[i]+ "  "+ "Age"+"\t"+"Height");
             	for(int j = 0; j < ageHeightDetails[i].length; j++){
-            		System.out.println(ageHeightDetails1[i][j]+"  ");
+            		System.out.print("\t"+ ageHeightDetails1[i][j]+ "  ");
             	}
             	
-            	System.out.println("-----------------------");
+            	System.out.println();
             }
             
             

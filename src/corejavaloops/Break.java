ca7a6fd96af4813b1c2b8f18c697b40bf87fc993
Sarah Fatima <sfatima81@gmail.com>
2017-04-01 11:31:16 -0500

    Add files via upload
    Java Assignment - 03/30/2017

---
+++ src/corejavaloops/Break.java
@@ -0,0 +1,22 @@
+package corejavaloops;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		int temperature [] = {40, 38, 35, 32, 30, 28};
+	
+		for (int i =0; i < temperature.length; i++) {
+			System.out.println("Temperature is a cool " + temperature[i]);
+					  	
+			if(i == 5 ){
+				break;
+		
+
+	}
+
+}
+	}
+		
+	
+	}
---
+++ src/corejavaloops/Continue.java
@@ -0,0 +1,21 @@
+package corejavaloops;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Even Numbers");
+
+        for(int i = 0; i <=20 ; i++){
+		     if(i % 2 == 1 ) continue;
+		       System.out.println(i);
+		       
+         
+		       	}
+
+
+	}
+
+}
+	
+

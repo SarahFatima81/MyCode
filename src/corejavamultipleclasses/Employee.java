0d70487e0b60eac201193bcb81f9809e62ea443b
Sarah Fatima <sfatima81@gmail.com>
2017-04-03 14:36:46 -0500

    Add files via upload
    Java Assignment - 04/03/2017

---
+++ src/corejavamultipleclasses/Employee.java
@@ -0,0 +1,43 @@
+package corejavamultipleclasses;
+
+public class Employee {
+
+			
+          public int number;
+          public String firstName;
+          public String gender;
+          private int age;
+          
+          public int getNumber(){
+        	  return number;
+          }
+          public void setNumber(int number){
+        	  this.number = number;
+          }
+          public String getFirstName() {
+			return firstName;
+		}
+		public void setFirstName(String firstName) {
+			this.firstName = firstName;
+		}
+		public String getGender() {
+			return gender;
+		}
+		public void setGender(String gender) {
+			this.gender = gender;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+           
+          
+          
+          
+          
+	}
+
+

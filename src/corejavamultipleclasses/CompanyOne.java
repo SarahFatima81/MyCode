ed289aac7ad3a4abbae6ff3d587407afa9ab6c2a
Sarah Fatima <sfatima81@gmail.com>
2017-04-03 20:23:25 -0500

    Add files via upload
    Commit changes

--- src/corejavamultipleclasses/CompanyOne.java
+++ src/corejavamultipleclasses/CompanyOne.java
@@ -7,7 +7,8 @@ public class CompanyOne {
 	public static void main(String[] args) {
 		
 				Scanner input = new Scanner(System.in);
-				Employee employees [] = new Employee [2];
+							
+				Employee employees [] = new Employee [3];
 				int num;
 				String name;
 				String gend;
@@ -17,27 +18,30 @@ public class CompanyOne {
 				
 				while(i < employees.length){
 					
-					System.out.println("Enter the Employee ID:   "+ "\t"+"Enter the Employee Name:   " + "\t"+ "Enter the Employee Gender:   " +"\t"+ "Enter the Employee Age: "); 
-					
+					System.out.println("Enter the Employee ID:   ");
 					num = input.nextInt();
+					System.out.println("Enter the Employee Name:   ");
 					name = input.next();
-					gend = input.next();
-					ageX = input.nextInt();
+				    System.out.println("Enter the Employee Gender:  ");
+				    gend = input.next();
+				    System.out.println("Enter the Employee Age: "); 
+				    ageX = input.nextInt();	
 					
+						
 					
+									
 					employees[i] = new Employee();
 					employees[i].setNumber(num);
 					employees[i].setFirstName(name);
 					employees[i].setGender(gend);
 					employees[i].setAge(ageX);
 					
-					System.out.println("Do you want to add more employees?");
-					String toExit = input.next();
-	                i++;
+					i++;
 					
 				}
-                     		
-              
+				
+             		
+	
 				for (i = 0; i < employees.length; i++){
 					
 					System.out.println("Employee ID: " + employees[i].getNumber() + "\n" + "Employee Name: " + employees[i].getFirstName() + "\n" + "Employee Gender: " + employees[i].getGender() + "\n" + "Employee Age: "+ employees[i].getAge());

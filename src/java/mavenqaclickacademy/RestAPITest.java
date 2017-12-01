2eab14b0301c36b5b786f11133d47ed51b3702a2
Sarah Fatima <sfatima81@gmail.com>
2017-11-30 19:04:05 -0600

    Add files via upload

---
+++ src/java/mavenqaclickacademy/RestAPITest.java
@@ -0,0 +1,18 @@
+package mavenqaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira(){
+		
+		System.out.println("Post Jira");
+	}
+	@Test
+	public void deleteTwitter(){
+		
+		System.out.println("Delete Twitter");
+	}
+
+}

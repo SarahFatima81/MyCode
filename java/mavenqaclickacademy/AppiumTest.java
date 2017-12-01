80503ed9e62884ab2313b6cb17d6c35821694110
Sarah Fatima <sfatima81@gmail.com>
2017-11-30 19:02:12 -0600

    Add files via upload

---
+++ java/mavenqaclickacademy/AppiumTest.java
@@ -0,0 +1,15 @@
+package mavenqaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeApps(){
+		System.out.println("Native Apps");
+	}
+    @Test
+    public void iOSApps(){
+    	System.out.println("iOS Apps");
+    }
+}
---
+++ java/mavenqaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package mavenqaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation(){
+		
+		System.out.println("Browser Automation");
+	}
+	
+	@Test
+    public void elementsUI(){
+    	
+    	System.out.println("Elements UI");
+    }
+}
